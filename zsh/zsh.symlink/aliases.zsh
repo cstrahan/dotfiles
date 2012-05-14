d45bae8fa1bd3e248edc0de3be14bc7d833bfb2d
Charles Strahan <charles.c.strahan@gmail.com>
2012-05-14 00:52:09 -0400

    Install fasd

--- zsh/zsh.symlink/aliases.zsh
+++ zsh/zsh.symlink/aliases.zsh
@@ -32,3 +32,11 @@ alias getch='git fetch'
 alias gitx='gitx --all'
 
 alias less='command less -R'
+
+# fasd
+alias a='fasd -a'    # any
+alias s='fasd -s'    # show / search / select
+alias d='fasd -d'    # directory
+alias f='fasd -f'    # file
+alias z='fasd_cd -d' # cd, same functionality as j in autojump
+alias v='f -e vim'   # quick opening files with vim
