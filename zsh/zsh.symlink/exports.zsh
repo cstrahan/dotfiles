1a8afb21671dcc641927fb731fb3f1333de03231
Charles Strahan <charles.c.strahan@gmail.com>
2012-05-13 22:55:03 -0400

    Syntax highlight with less

--- zsh/zsh.symlink/exports.zsh
+++ zsh/zsh.symlink/exports.zsh
@@ -18,3 +18,7 @@ export LESS_TERMCAP_se=$'\E[0m'           # end standout-mode
 export LESS_TERMCAP_so=$'\E[38;5;246m'    # begin standout-mode - info box
 export LESS_TERMCAP_ue=$'\E[0m'           # end underline
 export LESS_TERMCAP_us=$'\E[04;38;5;146m' # begin underline
+
+# syntax highlight code with less
+command -v src-hilite-lesspipe.sh >/dev/null 2>&1 &&
+  export LESSOPEN="| src-hilite-lesspipe.sh %s"
