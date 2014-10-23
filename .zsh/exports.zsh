d8d136149ec7dc2beb3ec3a71e5c9aa3f3ca8a3d
Charles Strahan <charles.c.strahan@gmail.com>
2014-10-23 03:43:45 +0200

    fix man page coloring

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -17,13 +17,14 @@ export CLICOLOR=1
 export LSCOLORS=gxfxcxdxbxegedabagacad
 
 # make man colorful
+export GROFF_NO_SGR=1
 export LESS_TERMCAP_mb=$'\E[01;31m'       # begin blinking
 export LESS_TERMCAP_md=$'\E[01;38;5;74m'  # begin bold
 export LESS_TERMCAP_me=$'\E[0m'           # end mode
 export LESS_TERMCAP_se=$'\E[0m'           # end standout-mode
 export LESS_TERMCAP_so=$'\E[1;31m'        # begin standout-mode - info box
 export LESS_TERMCAP_ue=$'\E[0m'           # end underline
-export LESS_TERMCAP_us=$'\E[04;33;5;146m' # begin underline
+export LESS_TERMCAP_us=$'\E[04;33;146m'   # begin underline
 
 # Cleaner Rails log entries
 export LOGRAGE_ENABLED=true
