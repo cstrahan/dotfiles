93374b2e12bb432726404242be128708b6376c4c
Charles Strahan <charles@cstrahan.com>
2023-08-07 19:32:31 -0500

    wezterm: tweaks

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,8 +5,30 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.bold_brightens_ansi_colors = true
+config.enable_kitty_keyboard = true
+config.use_resize_increments = true
+config.enable_scroll_bar = true
+
+-- disable ligatures
+config.harfbuzz_features = {
+  'calt=0',
+  'clig=0',
+  'liga=0',
+}
+
+-- blinking cursor
+-- config.default_cursor_style = 'BlinkingBlock'
+-- config.cursor_blink_ease_in = 'Constant'
+-- config.cursor_blink_ease_out = 'Constant'
+-- config.cursor_blink_rate = 600
+
 config.color_scheme = 'Tango (terminal.sexy)'
+-- make the scrollbar thumb more visible
+config.color_schemes = wezterm.get_builtin_color_schemes()
+local color_scheme = config.color_schemes[config.color_scheme]
+color_scheme.scrollbar_thumb = "555555"
+
+config.bold_brightens_ansi_colors = true
 config.font_size = 16
 config.font = wezterm.font_with_fallback {
   'JetBrains Mono',
@@ -18,6 +40,12 @@ local act = wezterm.action
 config.keys = {
   { key = 'UpArrow', mods = 'SHIFT', action = act.ScrollToPrompt(-1) },
   { key = 'DownArrow', mods = 'SHIFT', action = act.ScrollToPrompt(1) },
+}
+
+-- more visible tabs
+config.window_frame = {
+  font = wezterm.font { family = 'Roboto', weight = 'Bold' },
+  font_size = 14.0,
 }
 
 return config
