6699b64c5a31a2eb27ae78d6b9e45488ada4fb16
Charles Strahan <charles@cstrahan.com>
2024-08-29 16:32:39 -0500

    zim: update fzf

--- .zim/modules/fzf/README.md
+++ .zim/modules/fzf/README.md
@@ -1,8 +1,17 @@
 fzf
 ===
 
-Configures [fzf] to use [fd] or [ripgrep] for faster fetching of files and
-directories names.
+Sets up the Zsh shell integration for [fzf].
+
+The following should be removed from your `~/.zshrc`, since this module will
+already take care of it:
+
+```zsh
+source <(fzf --zsh)
+```
+
+Also configures [fzf] to use [bfs], [fd], [ripgrep] or [ugrep] for faster
+fetching of files and directories names.
 
 Sets ALT-C previews with ls. Also sets CTRL-T previews with [bat] if it's
 installed. Press CTRL-/ to toggle the preview.
@@ -10,10 +19,12 @@ installed. Press CTRL-/ to toggle the preview.
 Requirements
 ------------
 
-[fzf] must be installed already. And either [fd] or [ripgrep] must be installed
-already too.
+[fzf] >= 0.48.0 must be installed. And either [bfs], [fd], [ripgrep] or [ugrep]
+must be installed already too.
 
 [fzf]: https://github.com/junegunn/fzf
+[bfs]: https://tavianator.com/projects/bfs.html
 [fd]: https://github.com/sharkdp/fd
 [ripgrep]: https://github.com/BurntSushi/ripgrep
+[ugrep]: https://ugrep.com
 [bat]: https://github.com/sharkdp/bat
--- .zim/modules/fzf/init.zsh
+++ .zim/modules/fzf/init.zsh
@@ -1,21 +1,55 @@
-if (( ${+commands[fd]} )); then
-  export FZF_DEFAULT_COMMAND='command fd -H --no-ignore-vcs -E .git -tf'
+() {
+  local -r target=${1}
+  shift
+  if [[ ! ( -s ${target} && ${target} -nt ${commands[${1}]} ) ]]; then
+    "${@}" >! ${target} || return 1
+    zcompile -UR ${target}
+  fi
+  source ${target}
+} ${0:h}/fzf---zsh.zsh fzf --zsh || return 1
+
+if (( ${+commands[bfs]} )); then
+  export FZF_DEFAULT_COMMAND="command bfs -mindepth 1 -exclude -name .git -type d,f -printf '%P\n' 2>/dev/null"
+  export FZF_ALT_C_COMMAND="command bfs -mindepth 1 -exclude -name .git -type d -printf '%P\n' 2>/dev/null"
+  _fzf_compgen_path() {
+    command bfs ${1} -exclude -name .git -type d,f -a -not -path ${1} -print
+  }
+  _fzf_compgen_dir() {
+    command bfs ${1} -exclude -name .git -type d -a -not -path ${1} -print
+  }
+elif (( ${+commands[fd]} )); then
+  export FZF_DEFAULT_COMMAND='command fd -H --no-ignore-vcs -E .git -td -tf'
   export FZF_ALT_C_COMMAND='command fd -H --no-ignore-vcs -E .git -td'
   _fzf_compgen_path() {
-    command fd -H --no-ignore-vcs -E .git -tf . ${1}
+    command fd -H --no-ignore-vcs -E .git -td -tf . ${1}
   }
   _fzf_compgen_dir() {
     command fd -H --no-ignore-vcs -E .git -td . ${1}
   }
 elif (( ${+commands[rg]} )); then
-  export FZF_DEFAULT_COMMAND="command rg -uu -g '!.git' --files"
+  export FZF_DEFAULT_COMMAND="command rg -uu -g '!.git' --files --no-messages"
   _fzf_compgen_path() {
     command rg -uu -g '!.git' --files ${1}
   }
+elif (( ${+commands[ug]} )); then
+  export FZF_DEFAULT_COMMAND="command ug -lrs. --exclude-dir=.git --no-tree ''"
+  _fzf_compgen_path() {
+    command ug -lrs. --exclude-dir=.git --no-tree '' ${1}
+  }
 fi
+
+local bat_cmd
 if (( ${+commands[bat]} )); then
-  export FZF_CTRL_T_OPTS="--bind ctrl-/:toggle-preview --preview 'command bat --color=always --line-range :500 {}' ${FZF_CTRL_T_OPTS}"
+  bat_cmd=bat
+elif (( ${+commands[batcat]} )); then
+  # APT package
+  bat_cmd=batcat
 fi
+if [[ -n ${bat_cmd} ]]; then
+  export FZF_CTRL_T_OPTS="--bind ctrl-/:toggle-preview --preview 'command ${bat_cmd} --color=always --line-range :500 {}' ${FZF_CTRL_T_OPTS}"
+fi
+unset bat_cmd
+
 local ls_cmd
 if command ls --version &>/dev/null; then
   # GNU
