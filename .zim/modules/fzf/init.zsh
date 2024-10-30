9cb5a69a2038c1649fa3684c41e6d969b2edc5de
Charles Strahan <charles@cstrahan.com>
2024-10-30 13:37:58 -0500

    zsh: prevent annoying command not found message

--- .zim/modules/fzf/init.zsh
+++ .zim/modules/fzf/init.zsh
@@ -1,6 +1,7 @@
 () {
   local -r target=${1}
   shift
+  (( ${+commands[${1}]} )) || return 1
   if [[ ! ( -s ${target} && ${target} -nt ${commands[${1}]} ) ]]; then
     "${@}" >! ${target} || return 1
     zcompile -UR ${target}
