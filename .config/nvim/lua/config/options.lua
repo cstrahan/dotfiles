64cb989ea6ada210d780f7af489b959e318aad88
Charles Strahan <charles@cstrahan.com>
2025-09-15 17:30:52 -0500

    neovim: even better listchars

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+
 local opt = vim.opt
 -- set the terminal title
 opt.title = true
@@ -9,6 +10,7 @@ opt.title = true
 vim.g.snacks_animate = false
 vim.g.autoformat = false
 
-vim.opt.listchars = 'eol:¬,tab:▸ ,trail:·,nbsp:␣,extends:»,precedes:«'
+-- useful characters: (␍␤¬▸·␣»«⏎)
+vim.opt.listchars = 'eol:⏎,tab:▸ ,trail:·,nbsp:␣,extends:»,precedes:«'
 -- lazyvim defaults:
 --vim.opt.listchars = 'tab:> ,trail:-,nbsp:+'
